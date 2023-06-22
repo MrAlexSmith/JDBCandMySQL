42c2c7c8e99f381c6dc0257902a26f136c6bd171
MrAlexSmith <mr.alexander.smith@hotmail.com>
2023-06-22 18:07:20 +0300

    Задача с Hibernate решена.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,4 +1,4 @@
-package jm.task.core.jdbc.util;
+/*package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,5 +11,46 @@ public class Util {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
+*/
+
+package jm.task.core.jdbc.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/db_1_1_4";
+    private static final String JDBC_USERNAME = "root";
+    private static final String JDBC_PASSWORD = "1111dba";
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.url", JDBC_URL)
+                    .setProperty("hibernate.connection.username", JDBC_USERNAME)
+                    .setProperty("hibernate.connection.password", JDBC_PASSWORD)
+                    // Дополнительные настройки Hibernate...
+                    .configure();
+
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
